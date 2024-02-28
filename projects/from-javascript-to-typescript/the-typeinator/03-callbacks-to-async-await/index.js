628e3d35cce4e7a9f874d3d64e2d48955e68751a
Giorgio de la Barrera <giodelabarrera@gmail.com>
2024-02-28 17:54:12 +0100

    the typeinator

--- projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
+++ projects/from-javascript-to-typescript/the-typeinator/03-callbacks-to-async-await/index.js
@@ -1,5 +1,25 @@
 // Put your checkEmotion and speak functions here! ✨
 // See ./original.js for their older JavaScript code.
 
+function checkEmotion(knownEmotions, emotion) {
+	// Simulate database processing time by waiting a second...
+	return new Promise((res) => {
+		setTimeout(() => {
+			res(knownEmotions.has(emotion));
+		}, 1000);
+	});
+}
+
+function speak(knownEmotions, newEmotion, phrase) {
+	return new Promise(async (res, rej) => {
+		const hasEmotion = await checkEmotion(knownEmotions, newEmotion);
+		if (hasEmotion) {
+			res(`"${phrase}" (${newEmotion})`);
+		} else {
+			rej(new Error(`Does not compute. I do not understand ${newEmotion}.`));
+		}
+	});
+}
+
 module.exports.checkEmotion = checkEmotion;
 module.exports.speak = speak;
