1186b586f854ac8d1a6ec1f1694628b89e36a764
Giorgio de la Barrera <giodelabarrera@gmail.com>
2024-12-11 07:18:35 +0100

    Add analyzing dna array

--- projects/arrays/analyzing-dna/02-shallow-differences/index.ts
+++ projects/arrays/analyzing-dna/02-shallow-differences/index.ts
@@ -1,2 +1,14 @@
 // Write your shallowDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function shallowDifferences(a: string[], b: string[]) {
+	if (a.length !== b.length) return undefined;
+
+	const result = [];
+
+	for (let i = 0; i < a.length; i++) {
+		result.push(a[i] === b[i] ? a[i] : undefined);
+	}
+
+	return result;
+}
--- projects/arrays/analyzing-dna/04-deep-differences/index.ts
+++ projects/arrays/analyzing-dna/04-deep-differences/index.ts
@@ -1,2 +1,21 @@
 // Write your deepDifferences function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function deepDifferences(a: string[][], b: string[][]) {
+	const result: ((string | undefined)[] | undefined)[] = [];
+
+	for (let i = 0; i < a.length || i < b.length; i += 1) {
+		if (a[i].length !== b[i].length) {
+			result[i] = undefined;
+			continue;
+		}
+
+		const diff: (string | undefined)[] = [];
+		for (let j = 0; j < a[i].length; j += 1) {
+			diff.push(a[i][j] === b[i][j] ? a[i][j] : undefined);
+		}
+		result[i] = diff;
+	}
+
+	return result;
+}
