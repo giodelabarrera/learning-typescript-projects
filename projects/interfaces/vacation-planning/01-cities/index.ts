5203d2a25055fb64123765cb5419d526b01851c5
Giorgio de la Barrera <giodelabarrera@gmail.com>
2024-12-17 06:36:31 +0100

    Add interfaces 01 cities

--- projects/interfaces/vacation-planning/01-cities/index.ts
+++ projects/interfaces/vacation-planning/01-cities/index.ts
@@ -1,2 +1,42 @@
-// Write your describeCity function here! ✨
-// You'll need to export it so the tests can run it.
+interface City {
+	catchphrase?: string;
+	coordinates: Coordinates;
+	name: string;
+}
+
+interface Coordinates {
+	north: Coordinate;
+	west: Coordinate;
+}
+
+type Coordinate = [number, number, number];
+
+export function describeCity(city: City) {
+	const lines = [`${city.name}, New York`];
+
+	if (city.catchphrase) {
+		lines.push(`* "${city.catchphrase}"`);
+	}
+
+	lines.push(
+		[
+			`* Located at`,
+			`${describeCoordinate(city.coordinates.north)}N`,
+			`${describeCoordinate(city.coordinates.west)}W`,
+		].join(" ")
+	);
+
+	return lines.join("\n");
+}
+
+function describeCoordinate(coordinate: Coordinate) {
+	return [
+		`${describeUnit(coordinate[0])}°`,
+		`${describeUnit(coordinate[1])}'`,
+		`${describeUnit(coordinate[2])}"`,
+	].join("");
+}
+
+function describeUnit(value: number) {
+	return value.toString().padStart(2, "0");
+}
