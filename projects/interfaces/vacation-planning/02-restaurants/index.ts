effd648b6b701d26ca751a641864801c894fc812
Giorgio de la Barrera <giodelabarrera@gmail.com>
2024-12-18 06:57:10 +0100

    Add grouped exercise

--- projects/interfaces/vacation-planning/02-restaurants/index.ts
+++ projects/interfaces/vacation-planning/02-restaurants/index.ts
@@ -1,2 +1,18 @@
-// Write your groupRestaurants function here! ✨
-// You'll need to export it so the tests can run it.
+interface Restaurant {
+	city: string;
+	name: string;
+}
+
+interface GroupedRestaurants {
+	[city: string]: string[];
+}
+
+export function groupRestaurants(restaurants: Restaurant[]) {
+	return restaurants.reduce<GroupedRestaurants>((grouped, restaurant) => {
+		if (!grouped.hasOwnProperty(restaurant.city)) {
+			grouped[restaurant.city] = [];
+		}
+		grouped[restaurant.city].push(restaurant.name);
+		return grouped;
+	}, {});
+}
