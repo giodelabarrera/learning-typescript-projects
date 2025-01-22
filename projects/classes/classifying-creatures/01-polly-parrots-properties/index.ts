2d96bccc912e270be2a206b82ea960f1e9b458ea
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-01-22 06:43:59 +0100

    Add class polly exercise

--- projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
+++ projects/classes/classifying-creatures/01-polly-parrots-properties/index.ts
@@ -1,2 +1,24 @@
-// Write your Parrot class here! ✨
-// You'll need to export it so the tests can run it.
+class Parrot {
+	#name: string;
+	#phrases: string[];
+
+	constructor(name: string) {
+		this.#name = name;
+		this.#phrases = [`${this.#name} wants a cracker!`];
+	}
+
+	announce() {
+		return `Squawk! I'm ${this.#name}!`;
+	}
+
+	learn(word: string) {
+		this.#phrases.push(word);
+	}
+
+	speak() {
+		const random = Math.floor(Math.random() * this.#phrases.length);
+		return this.#phrases[random];
+	}
+}
+
+export { Parrot };
