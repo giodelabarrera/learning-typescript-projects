9b82ed8d5356e859a9433cc8406f024fa4c54b7a
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-01-22 17:03:30 +0100

    Add small furry pet

--- projects/classes/classifying-creatures/03-hamster-helpers/index.ts
+++ projects/classes/classifying-creatures/03-hamster-helpers/index.ts
@@ -1,2 +1,49 @@
-// Write your type and classes here! ✨
-// You'll need to export the classes so the tests can run them.
+export abstract class SmallFurryPet {
+	readonly species: string;
+	protected happiness = 0;
+
+	constructor(species: string) {
+		this.species = species;
+	}
+
+	abstract eats(food: SmallPetFood): boolean;
+
+	isHappy() {
+		return this.happiness > 0;
+	}
+}
+
+export class Gerbil extends SmallFurryPet {
+	constructor() {
+		super("Gerbil");
+	}
+
+	dig() {
+		this.happiness += 1;
+	}
+
+	eats(food: SmallPetFood): boolean {
+		return [`insects`, `plants`, `seeds`, `vegetables`].includes(food);
+	}
+}
+export class Hamster extends SmallFurryPet {
+	constructor() {
+		super("Hamster");
+	}
+
+	run() {
+		this.happiness += 1;
+	}
+
+	eats() {
+		return true;
+	}
+}
+
+export type SmallPetFood =
+	| `bugs`
+	| `fruits`
+	| `insects`
+	| `plants`
+	| `seeds`
+	| `vegetables`;
