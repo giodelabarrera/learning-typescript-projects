f1dcd29efa041207801f4ac72f5ca2f60e64835d
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-01-22 12:42:11 +0100

    Add classes dog display exercise

--- projects/classes/classifying-creatures/02-dog-displays/index.ts
+++ projects/classes/classifying-creatures/02-dog-displays/index.ts
@@ -1,2 +1,22 @@
-// Write your class and interface here! ✨
-// You'll need to export it so the tests can run it.
+interface PuppyInTheWindow {
+	readonly colors: string[];
+	readonly furriness: number;
+	readonly owner: string | undefined;
+}
+
+class Puppy implements PuppyInTheWindow {
+	colors: string[];
+	furriness: number;
+	owner: string | undefined;
+
+	constructor(colors: string[], furriness: number) {
+		this.colors = colors;
+		this.furriness = furriness;
+	}
+
+	adopt(owner: string) {
+		this.owner = owner;
+	}
+}
+
+export { Puppy };
