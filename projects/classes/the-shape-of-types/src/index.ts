bb5f2786be702da2756862f4b93de0814b2703c0
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-01-23 07:34:15 +0100

    Add solution for shape of types

--- projects/classes/the-shape-of-types/src/index.ts
+++ projects/classes/the-shape-of-types/src/index.ts
@@ -1,2 +1,66 @@
-// Write your classes here! ✨
-// You'll need to export them so the tests can run them.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+export abstract class Horror {
+	#consumedRecords: Consumed[] = [];
+
+	abstract readonly name: string;
+
+	#consume(opponent: Horror) {
+		this.#consumedRecords.push({
+			evil: opponent.isEvil(),
+			name: opponent.name,
+			power: opponent.getPower(),
+		});
+	}
+
+	doBattle(opponent: Horror) {
+		if (this.getPower() >= opponent.getPower()) {
+			this.#consume(opponent);
+		}
+	}
+
+	getPower(): number {
+		return this.#consumedRecords.reduce((accu, consumed) => {
+			return accu + this.getPowerFrom(consumed);
+		}, this.#consumedRecords.length);
+	}
+
+	protected abstract getPowerFrom(consumed: Consumed): number;
+
+	protected abstract isEvil(): boolean;
+}
+
+export class Demon extends Horror {
+	readonly name = "Demon";
+
+	getPowerFrom(consumed: Consumed): number {
+		return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+	}
+
+	isEvil(): boolean {
+		return true;
+	}
+}
+
+export class Sorcerer extends Horror {
+	readonly name;
+	#evil: boolean;
+
+	constructor(name: string, evil: boolean) {
+		super();
+		this.name = name;
+		this.#evil = evil;
+	}
+
+	protected getPowerFrom(consumed: Consumed) {
+		return consumed.evil === this.#evil ? consumed.power * 2 : consumed.power;
+	}
+
+	protected isEvil(): boolean {
+		return this.#evil;
+	}
+}
