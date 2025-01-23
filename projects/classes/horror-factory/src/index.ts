79cbe29a8c5e9738208641c0552925dff6fe1b04
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-01-23 11:52:31 +0100

    Add horror factory solution

--- projects/classes/horror-factory/src/index.ts
+++ projects/classes/horror-factory/src/index.ts
@@ -1,2 +1,70 @@
-// Write your class and functions here! ✨
-// You'll need to export the class and functions so the tests can run it.
+interface Consumed {
+	evil: boolean;
+	name: string;
+	power: number;
+}
+
+interface HorrorProperties {
+	name: string;
+	isEvil: () => boolean;
+	getPowerFrom: (consumed: Consumed) => number;
+}
+
+export class Horror {
+	#consumedRecords: Consumed[] = [];
+	name: string;
+	isEvil: () => boolean;
+	getPowerFrom: (consumed: Consumed) => number;
+
+	constructor(properties: HorrorProperties) {
+		this.name = properties.name;
+		this.isEvil = properties.isEvil;
+		this.getPowerFrom = properties.getPowerFrom;
+	}
+
+	#consume(opponent: Horror) {
+		this.#consumedRecords.push({
+			evil: opponent.isEvil(),
+			name: opponent.name,
+			power: opponent.getPower(),
+		});
+	}
+
+	doBattle(opponent: Horror) {
+		if (this.getPower() >= opponent.getPower()) {
+			this.#consume(opponent);
+		}
+	}
+
+	getPower(): number {
+		return this.#consumedRecords.reduce((accu, consumed) => {
+			return accu + this.getPowerFrom(consumed);
+		}, this.#consumedRecords.length);
+	}
+}
+
+export function createDemon() {
+	return new Horror({
+		name: "Demon",
+		isEvil() {
+			return true;
+		},
+		getPowerFrom(consumed) {
+			return consumed.evil ? consumed.power / 2 : consumed.power * 2;
+		},
+	});
+}
+
+export function createSorcerer(name: string, evil: boolean) {
+	return new Horror({
+		name,
+		isEvil() {
+			return evil;
+		},
+		getPowerFrom(consumed: Consumed) {
+			return consumed.evil === this.isEvil()
+				? consumed.power * 2
+				: consumed.power;
+		},
+	});
+}
