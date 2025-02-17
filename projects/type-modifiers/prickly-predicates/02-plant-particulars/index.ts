13e6a9fe2edb171000fb19195215f10e7ec27a9f
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-17 17:04:08 +0100

    Add 02 plant exercise

--- projects/type-modifiers/prickly-predicates/02-plant-particulars/index.ts
+++ projects/type-modifiers/prickly-predicates/02-plant-particulars/index.ts
@@ -6,3 +6,20 @@ export interface AnyCrop {
 
 // Write your isAnyCrop function here! ✨
 // You'll need to export it so the tests can run it.
+
+export function isAnyCrop(data: unknown): data is AnyCrop {
+	if (!!data && typeof data === "object") {
+		if (!("growth" in data)) return false;
+		if (!("harvested" in data)) return false;
+		if (!("name" in data)) return false;
+
+		if (typeof data.growth !== "number") return false;
+		if (typeof data.harvested !== "boolean") return false;
+		if (typeof data.name !== "string") return false;
+		if (!["cactus", "cassava", "chia"].includes(data.name)) return false;
+
+		return true;
+	}
+
+	return false;
+}
