beadea52ca10289477276e1a5e1e2597831e9cf8
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-17 11:06:10 +0100

    Add type modifier exercise

--- projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
+++ projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
@@ -1,4 +1,33 @@
 import { characters } from "./characters";
 
-// Write your announceCharacter function here! ✨
-// You'll need to export it so the tests can run it.
+type Power =
+	| "animal control"
+	| "strength"
+	| "magic"
+	| "mind control"
+	| "teleportation"
+	| "agility"
+	| "durability"
+	| "speed"
+	| "strength"
+	| "transformation"
+	| "telepathy"
+	| "telekinesis";
+
+type Side = "good" | "evil";
+
+interface Character {
+	name: string;
+	powers: Power[];
+	side: Side;
+}
+
+export function announceCharacter(raw: string) {
+	const character = JSON.parse(raw) as Character;
+
+	console.log(`I am ${character.name}.`);
+	console.log(`My powers are: ${character.powers.join(", ")}.`);
+	console.log(`I am ${character.side}.`);
+
+	return character;
+}
