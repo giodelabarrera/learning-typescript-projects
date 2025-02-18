e14b743e736b178bd51ae659845f86eac4a07ee8
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-18 11:48:26 +0100

    Add type force exercise

--- projects/type-modifiers/type-force/src/index.ts
+++ projects/type-modifiers/type-force/src/index.ts
@@ -1,32 +1,38 @@
-// Write your duel function and types below! ✨
-// You'll need to export duel so the tests can run it.
+interface Character {
+	flying: boolean;
+	name: string;
+	power: number;
+	toughness: number;
+}
 
 const mutationsLibrary = {
-	energy: (hero) => {
+	energy: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.flying = true;
 	},
-	healing: (hero) => {
+	healing: (hero: Character) => {
 		hero.toughness *= 2;
 	},
-	luck: (hero) => {
+	luck: (hero: Character) => {
 		hero.power *= 1.25;
 		hero.toughness *= 1.25;
 	},
-	flight: (hero) => {
+	flight: (hero: Character) => {
 		hero.flying = true;
 	},
-	strength: (hero) => {
+	strength: (hero: Character) => {
 		hero.power *= 2;
 	},
-	wings: (hero) => {
+	wings: (hero: Character) => {
 		hero.flying = true;
 		hero.toughness *= 0.9;
 	},
 };
 
-function createCharacter(name, mutations) {
-	const character = {
+type Mutation = keyof typeof mutationsLibrary;
+
+function createCharacter(name: string, mutations: Mutation[]) {
+	const character: Character = {
 		flying: false,
 		name,
 		power: 1,
@@ -38,4 +44,23 @@ function createCharacter(name, mutations) {
 	}
 
 	return character;
+}
+
+interface Fighter {
+	mutations: Mutation[];
+	name: string;
+}
+
+export function duel(good: Fighter, bad: Fighter) {
+	const goodCharacter = createCharacter(good.name, good.mutations);
+	const badCharacter = createCharacter(bad.name, bad.mutations);
+
+	const winner =
+		goodCharacter.power / badCharacter.toughness >
+		badCharacter.power / goodCharacter.toughness
+			? "hero"
+			: "villain";
+	const character = winner === "hero" ? goodCharacter : badCharacter;
+
+	return [winner, character] as const;
 }
