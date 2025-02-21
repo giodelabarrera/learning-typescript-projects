4aba41f235b8653daf24a9d38d9e203f6bf7d73c
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-21 17:07:26 +0100

    Add pick many exercise

--- projects/generics/hidash/04-pickmany/index.ts
+++ projects/generics/hidash/04-pickmany/index.ts
@@ -1,2 +1,5 @@
-// Write your pickMany function here! ✨
-// You'll need to export it so the tests can run it.
+export function pickMany<T, K extends keyof T>(container: T, keys: K[]) {
+	return keys.map((key) => container[key]);
+}
+
+pickMany({ a: 1, b: 2, c: 3 }, ["a", "c"]);
