9adac5333800fe14d84201bd59ee49754f8a979b
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-21 16:48:34 +0100

    Add generic zip function

--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,15 @@
-// Write your zip function here! ✨
-// You'll need to export it so the tests can run it.
+export function zip<T, U>(a: T[], b: U[]): (T | U)[] {
+	const result: (T | U)[] = [];
+
+	const maxLength = Math.max(a.length, b.length);
+	for (let i = 0; i < maxLength; i++) {
+		if (a[i]) {
+			result.push(a[i]);
+		}
+		if (b[i]) {
+			result.push(b[i]);
+		}
+	}
+
+	return result;
+}
