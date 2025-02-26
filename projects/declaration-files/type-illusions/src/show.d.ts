07777c37264d6081a246436069026f62adfee486
Giorgio de la Barrera <giodelabarrera@gmail.com>
2025-02-26 13:00:01 +0100

    Add declaration file type illusions

--- projects/declaration-files/type-illusions/src/show.d.ts
+++ projects/declaration-files/type-illusions/src/show.d.ts
@@ -1,1 +1,36 @@
-// Declare your types here! ✨
+export interface Trick {
+	gimmick: string;
+}
+
+export interface Illusion {
+	title: string;
+	duration: number;
+	introduction: () => string;
+	flair: () => string;
+	payoff: () => string;
+}
+
+export interface AudienceMember {
+	name: string;
+}
+
+export type Section = Illusion | Trick;
+
+export interface Act {
+	performer: string;
+	name: string;
+	sections: Section[];
+}
+
+interface GetAudienceMemberOptions {
+	duration: number;
+	title: string;
+}
+
+export function getAudienceMemberFor(
+	options: GetAudienceMemberOptions
+): Promise<AudienceMember>;
+
+export function isTrick(section: Section): section is Trick;
+
+export function isVolunteerIllusion(section: Section): section is Illusion;
